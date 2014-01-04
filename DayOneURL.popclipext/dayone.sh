e92f3482c6b45484aeb338e3680ca581b235dbc6
Brock Boland <brock@brockboland.com>
2014-01-04 15:21:50 -0600

    Put selected text into blockquote

--- DayOneURL.popclipext/dayone.sh
+++ DayOneURL.popclipext/dayone.sh
@@ -1,19 +1,31 @@
+# Split the selected text into lines to format as blockquote
+SELECTED_TEXT=""
+# Using \n for newline character doesn't work for some reason
+export IFS="
+"
+# For each line: prepend with > then add a blank > line for spacing
+for line in $POPCLIP_TEXT; do
+  SELECTED_TEXT="$SELECTED_TEXT> $line
+>
+"
+done
+
+
 if [ -z "$POPCLIP_BROWSER_URL" ]
 then
-  ENTRY_TEXT="$POPCLIP_TEXT"
+  # No browser URL: just use the selected text
+  ENTRY_TEXT="$SELECTED_TEXT"
 else
-  # Make sure that the title is set
+  # YES browser URL: Make sure that the title is set
   if [ -z "$POPCLIP_BROWSER_TITLE" ]
   then
     POPCLIP_BROWSER_TITLE=POPCLIP_BROWSER_URL
   fi
 
+  # Make the URL a link, and append the selected text
   ENTRY_TEXT="From [$POPCLIP_BROWSER_TITLE]($POPCLIP_BROWSER_URL):
-$POPCLIP_TEXT"
+$SELECTED_TEXT"
 fi
-
-echo "ENTRY_TEXT" >> /Users/bboland/Desktop/popclip.log
-echo "$ENTRY_TEXT" >> /Users/bboland/Desktop/popclip.log
 
 open "dayone://post?entry=$ENTRY_TEXT"
 open -b com.dayoneapp.dayone
